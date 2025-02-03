ac8f220ad7025afc651bf08ce8419838b9a252b4
artienda-mary-joyce-martin <joyce.art25@gmail.com>
2025-02-03 05:25:27 +0000

    add links to index

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Links-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!--title and logo-->
+    <link href="img/logo/attnlg.jpg" rel="icon">
     <title>Class Management System</title>
+
+    
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
 </head>
 <body>
     
