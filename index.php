bbeb9553de7a737cd26d05aad4b4868fa2eab1c1
artienda-mary-joyce-martin <joyce.art25@gmail.com>
2025-02-03 05:39:38 +0000

    add logo

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
     <meta name="author" content="">
 
     <!--title and logo-->
-    <link href="img/logo/attnlg.jpg" rel="icon">
+    <link href="img/logo/class_logo.png" rel="icon">
     <title>Class Management System</title>
 
-    
+
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="css/ruang-admin.min.css" rel="stylesheet">
