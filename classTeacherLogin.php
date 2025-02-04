d1a1b0181dfe3c665b40948c3fb565406583ab4a
Jhon Vianey <jhonvianeydelapenabulanadi04@gmail.com>
2025-02-03 19:09:39 -1200

    commit by

--- classTeacherLogin.php
+++ classTeacherLogin.php
@@ -22,78 +22,100 @@ session_start();
     </head>
 
     <body>
-    <div class="container-login">
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card shadow-sm my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="login-form">
-                                    <div class="text-center">
-                                        <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
-                                        <br><br>
-                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
-                                    </div>
+        <div class="container-login">
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card shadow-sm my-5">
+                        <div class="card-body p-0">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="login-form">
+                                        <div class="text-center">
+                                            <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
+                                            <br><br>
+                                            <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                        </div>
 
-                                    <form class="user" method="Post" action="">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                
+                                        <form class="user" method="Post" action="">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
                                             </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-primary btn-block" value="Login" name="login" />
-                                        </div>
-                                    </form>
+                                            <div class="form-group">
+                                                <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
+                                            </div>
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
+                                                    <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                    
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="submit" class="btn btn-primary btn-block" value="Login" name="login" />
+                                            </div>
+                                        </form>
 
-                                    <?php
+                                        <?php
 
-                                        if(isset($_POST['login'])){
+                                            if(isset($_POST['login'])){
 
-                                            $username = $_POST['username'];
-                                            $password = $_POST['password'];
+                                                $username = $_POST['username'];
+                                                $password = $_POST['password'];
 
-                                            $password = md5($password);
+                                                $password = md5($password);
 
-                                            $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
-                                            $rs = $conn->query($query);
-                                            $num = $rs->num_rows;
-                                            $rows = $rs->fetch_assoc();
+                                                $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
+                                                $rs = $conn->query($query);
+                                                $num = $rs->num_rows;
+                                                $rows = $rs->fetch_assoc();
 
-                                            if($num > 0){
+                                                if($num > 0){
 
-                                            $_SESSION['userId'] = $rows['Id'];
-                                            $_SESSION['firstName'] = $rows['firstName'];
-                                            $_SESSION['lastName'] = $rows['lastName'];
-                                            $_SESSION['emailAddress'] = $rows['emailAddress'];
-                                            $_SESSION['classId'] = $rows['classId'];
-                                            $_SESSION['classArmId'] = $rows['classArmId'];
+                                                $_SESSION['userId'] = $rows['Id'];
+                                                $_SESSION['firstName'] = $rows['firstName'];
+                                                $_SESSION['lastName'] = $rows['lastName'];
+                                                $_SESSION['emailAddress'] = $rows['emailAddress'];
+                                                $_SESSION['classId'] = $rows['classId'];
+                                                $_SESSION['classArmId'] = $rows['classArmId'];
 
-                                            echo "<script type = \"text/javascript\">
-                                            window.location = (\"ClassTeacher/index.php\")
-                                            </script>";
+                                                echo "<script type = \"text/javascript\">
+                                                window.location = (\"ClassTeacher/index.php\")
+                                                </script>";
+                                                }
+
+                                                else{
+
+                                                echo "<div class='alert alert-danger' role='alert'>
+                                                Invalid Username/Password!
+                                                </div>";
+
+                                                }
                                             }
+                                        ?>
 
-                                            else{
+                                     <!-- <hr>
+                                        <a href="index.html" class="btn btn-google btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a> -->
 
-                                            echo "<div class='alert alert-danger' role='alert'>
-                                            Invalid Username/Password!
-                                            </div>";
-
-                                            }
-                                        }
-                                    ?>
-
-
-
+                                            <hr>
+                                        <div class="text-center">
+                                            <a class="font-weight-bold small" href="classTeacherLogin.php">Class Teacher Login!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                             <!-- <a class="font-weight-bold small" href=".php">Cooperative Account!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                            <a class="font-weight-bold small" href="forgotPassword.php">Forgot Password?</a> -->
+                                        </div>
+                                        <div class="text-center">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
         
 </html>                                 
