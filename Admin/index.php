d240e7dd0e7c332c19a99fbefbc49cbc1eeaf008
Dextergomez <dextergomez1009@gmail.com>
2025-02-04 14:29:16 +0800

    adding admin/index.php

---
+++ Admin/index.php
@@ -0,0 +1,64 @@
+
+<?php 
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+    $query = "SELECT tblclass.className,tblclassarms.classArmName 
+    FROM tblclassteacher
+    INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+    INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId
+    Where tblclassteacher.Id = '$_SESSION[userId]'";
+
+    $rs = $conn->query($query);
+    $num = $rs->num_rows;
+    $rrw = $rs->fetch_assoc();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/class_logo.png" rel="icon">
+  <title>CMS - Dashboard</title>
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/ruang-admin.min.css" rel="stylesheet">
+
+
+  </head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+   <?php include "Includes/sidebar.php";?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+           <?php include "Includes/topbar.php";?>
+        <!-- Topbar -->
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Administrator Dashboard</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+            </ol>
+          </div>
+
+          <div class="row mb-3">
+          <!-- Students Card -->
+          <?php 
+$query1=mysqli_query($conn,"SELECT * from tblstudents");                       
+$students = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
