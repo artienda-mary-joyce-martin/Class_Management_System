13459b009d2564c6ae4b0036ef1d31350adb4522
Dextergomez <dextergomez1009@gmail.com>
2025-02-04 14:33:42 +0800

    adding query

--- Admin/index.php
+++ Admin/index.php
@@ -104,3 +104,26 @@ $class = mysqli_num_rows($query1);
               </div>
             </div>
             <!-- Class Arm Card -->
+            <?php 
+$query1=mysqli_query($conn,"SELECT * from tblclassarms");                       
+$classArms = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Class Arms</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $classArms;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                        <span>Since last years</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-code-branch fa-2x text-success"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
