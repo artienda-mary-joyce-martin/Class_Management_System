5a660e9706fcbd505e00961d159329e7c0d70f98
Dextergomez <dextergomez1009@gmail.com>
2025-02-04 14:31:46 +0800

    adding class card

--- Admin/index.php
+++ Admin/index.php
@@ -62,3 +62,45 @@ $students = mysqli_num_rows($query1);
             <div class="col-xl-3 col-md-6 mb-4">
               <div class="card h-100">
                 <div class="card-body">
+
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Students</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $students;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+                        <span>Since last month</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-info"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Class Card -->
+             <?php 
+$query1=mysqli_query($conn,"SELECT * from tblclass");                       
+$class = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Classes</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $class;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                        <span>Since last month</span> -->
+                        </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-chalkboard fa-2x text-primary"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Class Arm Card -->
