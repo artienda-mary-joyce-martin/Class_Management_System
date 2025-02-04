c721851203fd33b51dc98135de15e241bd39f20e
Jhon Vianey <jhonvianeydelapenabulanadi04@gmail.com>
2025-02-03 17:05:57 -1200

    adding login

---
+++ classTeacherLogin.php
@@ -0,0 +1,42 @@
+<?php 
+include 'Includes/dbcon.php';
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link href="img/logo/class_logo.png" rel="icon">
+        <title>Class Management System - Login</title>
+        <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <link href="css/ruang-admin.min.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+    <div class="container-login">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card shadow-sm my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="login-form">
+                                    <div class="text-center">
+                                        <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
+                                        <br><br>
+                                        <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                    </div>
+                                    
+
+    </body>
+        
+</html>                                 
