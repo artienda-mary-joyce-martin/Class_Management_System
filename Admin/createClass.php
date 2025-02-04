330595cfb53347cbb924926f2f15f90e9f03915e
culala-andrea <andreaculala78@gmail.com>
2025-02-04 15:44:49 +0800

    Update Admin
    commit by andrea

---
+++ Admin/createClass.php
@@ -0,0 +1,33 @@
+<?php 
+error_reporting(0);
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+//------------------------SAVE--------------------------------------------------
+
+if(isset($_POST['save'])){
+    
+    $className=$_POST['className'];
+   
+    $query=mysqli_query($conn,"select * from tblclass where className ='$className'");
+    $ret=mysqli_fetch_array($query);
+
+    if($ret > 0){ 
+
+        $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>This Class Already Exists!</div>";
+    }
+    else{
+
+        $query=mysqli_query($conn,"insert into tblclass(className) value('$className')");
+
+    if ($query) {
+        
+        $statusMsg = "<div class='alert alert-success'  style='margin-right:700px;'>Created Successfully!</div>";
+    }
+    else
+    {
+         $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>";
+    }
+  }
+}
+
