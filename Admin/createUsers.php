e084788c6481c946103a5bd74616f21560e08393
Jhon Vianey <jhonvianeydelapenabulanadi04@gmail.com>
2025-02-04 21:12:23 -1200

    add script

--- Admin/createUsers.php
+++ Admin/createUsers.php
@@ -1,7 +1,7 @@
 <?php 
-error_reporting(0);
-include '../Includes/dbcon.php';
-include '../Includes/session.php';
+    error_reporting(0);
+    include '../Includes/dbcon.php';
+    include '../Includes/session.php';
 
   if (isset($_GET['status'])){
     $status = $_GET['status'];
@@ -19,3 +19,50 @@ include '../Includes/session.php';
     }
                             
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="../img/logo/class_png" rel="icon">
+    <title>Class Management System - Dashboard</title>
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
+
+
+    <script>
+
+        function displayCompany(str) {
+            if (str == "") {
+                document.getElementById("txtHintt").innerHTML = "";
+                return;
+            } else { 
+                if (window.XMLHttpRequest) {
+                    // code for IE7+, Firefox, Chrome, Opera, Safari
+                    xmlhttp = new XMLHttpRequest();
+                } else {
+                    // code for IE6, IE5
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                }
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("txtHintt").innerHTML = this.responseText;
+                    }
+                };
+                xmlhttp.open("GET","ajaxforCompany2.php?qc="+str,true);
+                xmlhttp.send();
+            }
+        }
+    </script>	
+    </head>
+
+    <body>
+
+    </body>
+ </html>   
