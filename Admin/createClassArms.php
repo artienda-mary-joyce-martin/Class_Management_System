48aea4da709565fd18b83cf1ed94b3bb4392c248
culala-andrea <andreaculala78@gmail.com>
2025-02-05 16:06:42 +0800

    Update createClassArms.php
    commit by andrea

--- Admin/createClassArms.php
+++ Admin/createClassArms.php
@@ -86,3 +86,55 @@ if (isset($_GET['Id']) && isset($_GET['action']) && $_GET['action'] == "edit")
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/class_logo.png" rel="icon">
+<?php include 'includes/title.php';?>
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/ruang-admin.min.css" rel="stylesheet">
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+      <?php include "Includes/sidebar.php";?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+       <?php include "Includes/topbar.php";?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Create Class Arms</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Create Class Arms</li>
+            </ol>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- Form Basic -->
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Create Class Arms</h6>
+                    <?php echo $statusMsg; ?>
+                </div>
+                <div class="card-body">
+                  <form method="post">
+                    <div class="form-group row mb-3">
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Select Class<span class="text-danger ml-2">*</span></label>
+ <?php
