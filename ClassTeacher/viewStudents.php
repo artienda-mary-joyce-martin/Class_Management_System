d9e4a4e3f97342f736c4ac6eb0e401f13a601d0c
Dextergomez <dextergomez1009@gmail.com>
2025-02-05 20:19:33 +0800

    commited by dexter

--- ClassTeacher/viewStudents.php
+++ ClassTeacher/viewStudents.php
@@ -55,3 +55,114 @@ $query = "SELECT tblclass.className,tblclassarms.classArmName
     }
 }
 </script>
+
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+      <?php include "Includes/sidebar.php";?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+       <?php include "Includes/topbar.php";?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">All Student in (<?php echo $rrw['className'].' - '.$rrw['classArmName'];?>) Class</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">All Student in Class</li>
+            </ol>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- Form Basic -->
+
+
+              <!-- Input Group -->
+                 <div class="row">
+              <div class="col-lg-12">
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">All Student In Class</h6>
+                </div>
+                <div class="table-responsive p-3">
+                  <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Other Name</th>
+                        <th>Admission No</th>
+                        <th>Class</th>
+                        <th>Class Arm</th>
+                      </tr>
+                    </thead>
+                    
+                    <tbody>
+
+                  <?php
+                      $query = "SELECT tblstudents.Id,tblclass.className,tblclassarms.classArmName,tblclassarms.Id AS classArmId,tblstudents.firstName,
+                      tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber,tblstudents.dateCreated
+                      FROM tblstudents
+                      INNER JOIN tblclass ON tblclass.Id = tblstudents.classId
+                      INNER JOIN tblclassarms ON tblclassarms.Id = tblstudents.classArmId
+                      where tblstudents.classId = '$_SESSION[classId]' and tblstudents.classArmId = '$_SESSION[classArmId]'";
+                      $rs = $conn->query($query);
+                      $num = $rs->num_rows;
+                      $sn=0;
+                      $status="";
+                      if($num > 0)
+                      { 
+                        while ($rows = $rs->fetch_assoc())
+                          {
+                             $sn = $sn + 1;
+                            echo"
+                              <tr>
+                                <td>".$sn."</td>
+                                <td>".$rows['firstName']."</td>
+                                <td>".$rows['lastName']."</td>
+                                <td>".$rows['otherName']."</td>
+                                <td>".$rows['admissionNumber']."</td>
+                                <td>".$rows['className']."</td>
+                                <td>".$rows['classArmName']."</td>
+                              </tr>";
+                          }
+                      }
+                      else
+                      {
+                           echo   
+                           "<div class='alert alert-danger' role='alert'>
+                            No Record Found!
+                            </div>";
+                      }
+                      
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <!--Row-->
+
+          <!-- Documentation Link -->
+          <!-- <div class="row">
+            <div class="col-lg-12 text-center">
+              <p>For more documentations you can visit<a href="https://getbootstrap.com/docs/4.3/components/forms/"
+                  target="_blank">
+                  bootstrap forms documentations.</a> and <a
+                  href="https://getbootstrap.com/docs/4.3/components/input-group/" target="_blank">bootstrap input
+                  groups documentations</a></p>
+            </div>
+          </div> -->
+
+        </div>
+        <!---Container Fluid-->
