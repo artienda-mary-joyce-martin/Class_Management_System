94c88bf4714447feb4f8609f0c692d08662b6b3f
Jhon Vianey <79180064+bulanadi-jhonvianey@users.noreply.github.com>
2025-02-05 00:03:43 -1200

    add script

--- ClassTeacher/takeAttendance.php
+++ ClassTeacher/takeAttendance.php
@@ -89,3 +89,43 @@
 
 
 ?>
+    <!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="img/logo/class_logo.png" rel="icon">
+    <title>Class Management System - Dashboard</title>
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
+  
+  
+  
+       <script>
+        function classArmDropdown(str) {
+        if (str == "") {
+            document.getElementById("txtHint").innerHTML = "";
+            return;
+        } else { 
+            if (window.XMLHttpRequest) {
+                // code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                // code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("txtHint").innerHTML = this.responseText;
+                }
+            };
+            xmlhttp.open("GET","ajaxClassArms2.php?cid="+str,true);
+            xmlhttp.send();
+        }
+    }
+    </script>
