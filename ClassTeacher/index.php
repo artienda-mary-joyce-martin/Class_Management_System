af4bcace1e43d290248ce922a8eb7cf914261d4a
timbolchristianflorence <timbolchristianflorence@gmail.com>
2025-02-05 20:06:16 +0800

    commited by christian

--- ClassTeacher/index.php
+++ ClassTeacher/index.php
@@ -51,3 +51,35 @@ include '../Includes/session.php';
             </ol>
           </div>
 
+
+          <div class="row mb-3">
+          <!-- New User Card Example -->
+          <?php 
+$query1=mysqli_query($conn,"SELECT * from tblstudents where classId = '$_SESSION[classId]' and classArmId = '$_SESSION[classArmId]'");                       
+$students = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Students</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $students;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+                        <span>Since last month</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-info"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Earnings (Monthly) Card Example -->
+             <?php 
+$query1=mysqli_query($conn,"SELECT * from tblclass");                       
+$class = mysqli_num_rows($query1);
+?>
+
