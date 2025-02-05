7ea58a0b786509fc0ba5779e4d8587149d1161ab
Jhon Vianey <jhonvianeydelapenabulanadi04@gmail.com>
2025-02-04 21:06:21 -1200

    adding php

---
+++ Admin/createUsers.php
@@ -0,0 +1,21 @@
+<?php 
+error_reporting(0);
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+  if (isset($_GET['status'])){
+    $status = $_GET['status'];
+    $statusMsg = "";
+    if($status == "success"){
+          $statusMsg = "<div class='alert alert-success'  style='margin-right:700px;'>Created Successfully!</div>";
+    }
+    if($status == "fail"){
+          $statusMsg = "<div class='alert alert-danger'  style='margin-right:700px;'>An Error Occurred!</div>";
+    }
+    if($status == "exists"){
+          $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>Already Exists!</div>";
+    }
+
+    }
+                            
+?>
