31ebad49827b1aad3938b9e1f027203543a9e30d
timbolchristianflorence <timbolchristianflorence@gmail.com>
2025-02-05 17:35:02 +0800

    added by christian

--- Admin/createClassTeacher.php
+++ Admin/createClassTeacher.php
@@ -49,3 +49,121 @@ if(isset($_POST['save'])){
     }
   }
 }
+//---------------------------------------EDIT-------------------------------------------------------------
+
+
+
+
+
+
+//--------------------EDIT------------------------------------------------------------
+
+if (isset($_GET['Id']) && isset($_GET['action']) && $_GET['action'] == "edit")
+{
+    $Id= $_GET['Id'];
+
+    $query=mysqli_query($conn,"select * from tblclassteacher where Id ='$Id'");
+    $row=mysqli_fetch_array($query);
+
+    //------------UPDATE-----------------------------
+
+    if(isset($_POST['update'])){
+
+         $firstName=$_POST['firstName'];
+          $lastName=$_POST['lastName'];
+          $emailAddress=$_POST['emailAddress'];
+
+          $phoneNo=$_POST['phoneNo'];
+          $classId=$_POST['classId'];
+          $classArmId=$_POST['classArmId'];
+          $dateCreated = date("Y-m-d");
+
+$query=mysqli_query($conn,"update tblclassteacher set firstName='$firstName', lastName='$lastName',
+emailAddress='$emailAddress', password='$password',phoneNo='$phoneNo', classId='$classId',classArmId='$classArmId'
+where Id='$Id'");
+        if ($query) {
+            
+            echo "<script type = \"text/javascript\">
+            window.location = (\"createClassTeacher.php\")
+            </script>"; 
+        }
+        else
+        {
+            $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>";
+        }
+    }
+}
+
+
+//--------------------------------DELETE------------------------------------------------------------------
+
+if (isset($_GET['Id']) && isset($_GET['classArmId']) && isset($_GET['action']) && $_GET['action'] == "delete")
+{
+    $Id= $_GET['Id'];
+    $classArmId= $_GET['classArmId'];
+
+    $query = mysqli_query($conn,"DELETE FROM tblclassteacher WHERE Id='$Id'");
+
+    if ($query == TRUE) {
+
+        $qu=mysqli_query($conn,"update tblclassarms set isAssigned='0' where Id ='$classArmId'");
+        if ($qu) {
+            
+             echo "<script type = \"text/javascript\">
+            window.location = (\"createClassTeacher.php\")
+            </script>"; 
+        }
+        else
+        {
+            $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>";
+        }
+    }
+    else{
+
+        $statusMsg = "<div class='alert alert-danger' style='margin-right:700px;'>An error Occurred!</div>"; 
+     }
+  
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link href="img/logo/class_logo.png" rel="icon">
+<?php include 'includes/title.php';?>
+<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+<link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/ruang-admin.min.css" rel="stylesheet">
+
+
+
+<script>
+function classArmDropdown(str) {
+if (str == "") {
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+} else { 
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("txtHint").innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("GET","ajaxClassArms.php?cid="+str,true);
+    xmlhttp.send();
+}
+}
