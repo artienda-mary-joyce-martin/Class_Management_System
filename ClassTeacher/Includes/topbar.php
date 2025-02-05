f1c0db82726205b44776eabccf7488d44d852add
Dextergomez <dextergomez1009@gmail.com>
2025-02-05 21:47:33 +0800

    commited by dexter

--- ClassTeacher/Includes/topbar.php
+++ ClassTeacher/Includes/topbar.php
@@ -105,3 +105,116 @@ $query = "SELECT tblclass.className,tblclassarms.classArmName
                     </thead>
                     
                     <tbody>
+                    <?php
+                      $query = "SELECT tblstudents.Id,tblclass.className,tblclassarms.classArmName,tblclassarms.Id AS classArmId,tblstudents.firstName,
+                      tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber,tblstudents.dateCreated
+                      FROM tblstudents
+                      INNER JOIN tblclass ON tblclass.Id = tblstudents.classId
+                      INNER JOIN tblclassarms ON tblclassarms.Id = tblstudents.classArmId
+                      where tblstudents.classId = '$_SESSION[classId]' and tblstudents.classArmId = '$_SESSION[classArmId]'";
+                      $rs = $conn->query($query);
+                      $num = $rs->num_rows;
+                      $sn=0;
+                      $status="";
+                      if($num > 0)
+                      { 
+                        while ($rows = $rs->fetch_assoc())
+                          {
+                             $sn = $sn + 1;
+                            echo"
+                              <tr>
+                                <td>".$sn."</td>
+                                <td>".$rows['firstName']."</td>
+                                <td>".$rows['lastName']."</td>
+                                <td>".$rows['otherName']."</td>
+                                <td>".$rows['admissionNumber']."</td>
+                                <td>".$rows['className']."</td>
+                                <td>".$rows['classArmName']."</td>
+                              </tr>";
+                          }
+                      }
+                      else
+                      {
+                           echo   
+                           "<div class='alert alert-danger' role='alert'>
+                            No Record Found!
+                            </div>";
+                      }
+                      
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <!--Row-->
+
+          <!-- Documentation Link -->
+          <!-- <div class="row">
+            <div class="col-lg-12 text-center">
+              <p>For more documentations you can visit<a href="https://getbootstrap.com/docs/4.3/components/forms/"
+                  target="_blank">
+                  bootstrap forms documentations.</a> and <a
+                  href="https://getbootstrap.com/docs/4.3/components/input-group/" target="_blank">bootstrap input
+                  groups documentations</a></p>
+            </div>
+          </div> -->
+
+        </div>
+        <!---Container Fluid-->
+     <?php 
+
+  $query = "SELECT * FROM tblclassteacher WHERE Id = ".$_SESSION['userId']."";
+  $rs = $conn->query($query);
+  $num = $rs->num_rows;
+  $rows = $rs->fetch_assoc();
+  $fullName = $rows['firstName']." ".$rows['lastName'];;
+
+?>
+<nav class="navbar navbar-expand navbar-light bg-gradient-primary topbar mb-4 static-top">
+          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+        <div class="text-white big" style="margin-left:100px;"></div>
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                aria-labelledby="searchDropdown">
+                <form class="navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-1 small" placeholder="What do you want to look for?"
+                      aria-label="Search" aria-describedby="basic-addon2" style="border-color: #3f51b5;">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+         
+           
+            <div class="topbar-divider d-none d-sm-block"></div>
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <img class="img-profile rounded-circle" src="img/user-icn.png" style="max-width: 60px">
+                <span class="ml-2 d-none d-lg-inline text-white small"><b>Welcome <?php echo $fullName;?></b></span>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="logout.php">
+                <i class="fas fa-power-off fa-fw mr-2 text-danger"></i>
+                  Logout
+                </a>
+              </div>
+            </li>
+          </ul>
+        </nav>
