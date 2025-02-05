8f19b8ac4f3cbf034b5fc62f61dd690e1fd2848e
timbolchristianflorence <timbolchristianflorence@gmail.com>
2025-02-05 17:36:32 +0800

    added by christian

--- Admin/createClassTeacher.php
+++ Admin/createClassTeacher.php
@@ -167,3 +167,202 @@ if (str == "") {
     xmlhttp.send();
 }
 }
+</script>
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+      <?php include "Includes/sidebar.php";?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+       <?php include "Includes/topbar.php";?>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Create Class Teachers</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Create Class Teachers</li>
+            </ol>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-12">
+              <!-- Form Basic -->
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Create Class Teachers</h6>
+                    <?php echo $statusMsg; ?>
+                </div>
+                <div class="card-body">
+                  <form method="post">
+                   <div class="form-group row mb-3">
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Firstname<span class="text-danger ml-2">*</span></label>
+                        <input type="text" class="form-control" required name="firstName" value="<?php echo $row['firstName'];?>" id="exampleInputFirstName">
+                        </div>
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Lastname<span class="text-danger ml-2">*</span></label>
+                      <input type="text" class="form-control" required name="lastName" value="<?php echo $row['lastName'];?>" id="exampleInputFirstName" >
+                        </div>
+                    </div>
+                     <div class="form-group row mb-3">
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Email Address<span class="text-danger ml-2">*</span></label>
+                        <input type="email" class="form-control" required name="emailAddress" value="<?php echo $row['emailAddress'];?>" id="exampleInputFirstName" >
+                        </div>
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Phone No<span class="text-danger ml-2">*</span></label>
+                      <input type="text" class="form-control" name="phoneNo" value="<?php echo $row['phoneNo'];?>" id="exampleInputFirstName" >
+                        </div>
+                    </div>
+                    <div class="form-group row mb-3">
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Select Class<span class="text-danger ml-2">*</span></label>
+                         <?php
+                        $qry= "SELECT * FROM tblclass ORDER BY className ASC";
+                        $result = $conn->query($qry);
+                        $num = $result->num_rows;		
+                        if ($num > 0){
+                          echo ' <select required name="classId" onchange="classArmDropdown(this.value)" class="form-control mb-3">';
+                          echo'<option value="">--Select Class--</option>';
+                          while ($rows = $result->fetch_assoc()){
+                          echo'<option value="'.$rows['Id'].'" >'.$rows['className'].'</option>';
+                              }
+                                  echo '</select>';
+                              }
+                            ?>  
+                        </div>
+                        <div class="col-xl-6">
+                        <label class="form-control-label">Class Arm<span class="text-danger ml-2">*</span></label>
+                            <?php
+                                echo"<div id='txtHint'></div>";
+                            ?>
+                        </div>
+                    </div>
+                      <?php
+                    if (isset($Id))
+                    {
+                    ?>
+                    <button type="submit" name="update" class="btn btn-warning">Update</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <?php
+                    } else {           
+                    ?>
+                    <button type="submit" name="save" class="btn btn-primary">Save</button>
+                    <?php
+                    }         
+                    ?>
+                  </form>
+                </div>
+              </div>
+
+              <!-- Input Group -->
+                 <div class="row">
+              <div class="col-lg-12">
+              <div class="card mb-4">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">All Class Teachers</h6>
+                </div>
+                <div class="table-responsive p-3">
+                  <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email Address</th>
+                        <th>Phone No</th>
+                        <th>Class</th>
+                        <th>Class Arm</th>
+                        <th>Date Created</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+                   
+                    <tbody>
+
+                  <?php
+                      $query = "SELECT tblclassteacher.Id,tblclass.className,tblclassarms.classArmName,tblclassarms.Id AS classArmId,tblclassteacher.firstName,
+                      tblclassteacher.lastName,tblclassteacher.emailAddress,tblclassteacher.phoneNo,tblclassteacher.dateCreated
+                      FROM tblclassteacher
+                      INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+                      INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId";
+                      $rs = $conn->query($query);
+                      $num = $rs->num_rows;
+                      $sn=0;
+                      $status="";
+                      if($num > 0)
+                      { 
+                        while ($rows = $rs->fetch_assoc())
+                          {
+                             $sn = $sn + 1;
+                            echo"
+                              <tr>
+                                <td>".$sn."</td>
+                                <td>".$rows['firstName']."</td>
+                                <td>".$rows['lastName']."</td>
+                                <td>".$rows['emailAddress']."</td>
+                                <td>".$rows['phoneNo']."</td>
+                                <td>".$rows['className']."</td>
+                                <td>".$rows['classArmName']."</td>
+                                 <td>".$rows['dateCreated']."</td>
+                                <td><a href='?action=delete&Id=".$rows['Id']."&classArmId=".$rows['classArmId']."'><i class='fas fa-fw fa-trash'></i></a></td>
+                              </tr>";
+                          }
+                      }
+                      else
+                      {
+                           echo   
+                           "<div class='alert alert-danger' role='alert'>
+                            No Record Found!
+                            </div>";
+                      }
+                      
+                      ?>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+          <!--Row-->
+
+        </div>
+        <!---Container Fluid-->
+      </div>
+      <!-- Footer -->
+       <?php include "Includes/footer.php";?>
+      <!-- Footer -->
+    </div>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/ruang-admin.min.js"></script>
+   <!-- Page level plugins -->
+  <script src="../vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="../vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script>
+    $(document).ready(function () {
+      $('#dataTable').DataTable(); // ID From dataTable 
+      $('#dataTableHover').DataTable(); // ID From dataTable with Hover
+    });
+  </script>
+</body>
+
+</html>
