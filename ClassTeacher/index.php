b0869f4f6b4374946c5fcdc86673354f76896efc
timbolchristianflorence <timbolchristianflorence@gmail.com>
2025-02-05 20:03:59 +0800

    added index in classteacher

---
+++ ClassTeacher/index.php
@@ -0,0 +1,17 @@
+<?php 
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+
+    $query = "SELECT tblclass.className,tblclassarms.classArmName 
+    FROM tblclassteacher
+    INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+    INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId
+    Where tblclassteacher.Id = '$_SESSION[userId]'";
+
+    $rs = $conn->query($query);
+    $num = $rs->num_rows;
+    $rrw = $rs->fetch_assoc();
+
+
+?>
