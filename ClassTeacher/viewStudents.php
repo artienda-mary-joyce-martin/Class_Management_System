15a2e04d8dc077c2e7963113973b9c512dd70cbc
Dextergomez <dextergomez1009@gmail.com>
2025-02-05 20:18:36 +0800

    addinng php and link

---
+++ ClassTeacher/viewStudents.php
@@ -0,0 +1,57 @@
+<?php 
+error_reporting(0);
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+$query = "SELECT tblclass.className,tblclassarms.classArmName 
+    FROM tblclassteacher
+    INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+    INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId
+    Where tblclassteacher.Id = '$_SESSION[userId]'";
+
+    $rs = $conn->query($query);
+    $num = $rs->num_rows;
+    $rrw = $rs->fetch_assoc();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/class_logo.png" rel="icon">
+  <title>Class Management System - Dashboard</title>
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/ruang-admin.min.css" rel="stylesheet">
+
+
+
+   <script>
+    function classArmDropdown(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else { 
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","ajaxClassArms2.php?cid="+str,true);
+        xmlhttp.send();
+    }
+}
+</script>
